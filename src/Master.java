c80d3db5e3edcb036be5465583735092f7cb9b20
Ohiko444 <Ohiko444@yandex.ru>
2023-02-19 23:09:30 +0300

    master2: добавление вычесления функции

--- src/Master.java
+++ src/Master.java
@@ -1,5 +1,11 @@
 public class Master {
     public static void main(String[] args) {
+
         System.out.println("Hello, " + args[0]);
+
+        double x = Double.parseDouble(args[1]);
+        double y = Double.parseDouble(args[2]);
+
+        System.out.println((3*Math.cos(x-(Math.PI/6.0))*Math.cos(x-(Math.PI/6.0)))/(1/2.0+Math.sin(y*y)));
     }
 }
